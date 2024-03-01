c245b6dbc7e3b27ded7f8f374635e0566f6efba2
shkimmie-umb <sanghyuk.kim001@umb.edu>
2024-03-01 00:00:12 -0500

    meshnet

--- melanoma/cnn.py
+++ melanoma/cnn.py
@@ -272,6 +272,373 @@ class CNN(Base_Model):
 
         return model
 
+    def meshnet_kerasstyle(self, include_top=False, weights='imagenet', input_tensor=None, \
+        input_shape=None, pooling='max', classes=1000, **kwargs):
+
+        # from tensorflow.keras.applications import get_submodules_from_kwargs
+        # from tensorflow.keras.applications import imagenet_utils
+        # from tensorflow.keras.applications.imagenet_utils import decode_predictions
+        # from tensorflow.keras.applications.imagenet_utils import _obtain_input_shape
+        
+        
+        from keras.applications.imagenet_utils import obtain_input_shape
+        
+        input_shape = obtain_input_shape(input_shape,
+                                      default_size=224,
+                                      min_size=32,
+                                      data_format=tf.keras.backend.image_data_format(),
+                                      require_flatten=include_top,
+                                      weights=weights)
+
+        if input_tensor is None:
+            img_input = layers.Input(shape=input_shape)
+        else:
+            if not tf.keras.backend.is_keras_tensor(input_tensor):
+                img_input = layers.Input(tensor=input_tensor, shape=input_shape)
+            else:
+                img_input = input_tensor
+
+        # Block 1
+        x = layers.Conv2D(64, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block1_conv1')(img_input)
+        x = layers.Conv2D(64, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block1_conv2')(x)
+        x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block1_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 2
+        x = layers.Conv2D(128, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block2_conv1')(x)
+        x = layers.Conv2D(128, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block2_conv2')(x)
+        x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block2_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 3
+        x = layers.Conv2D(256, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(2, 2),
+                        name='block3_conv1')(x)
+        x = layers.Conv2D(256, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(2, 2),
+                        name='block3_conv2')(x)
+        x = layers.Conv2D(256, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(2, 2),
+                        name='block3_conv3')(x)
+        x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block3_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 4
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(4, 4),
+                        name='block4_conv1')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(4, 4),
+                        name='block4_conv2')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(4, 4),
+                        name='block4_conv3')(x)
+        # x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block4_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 5
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(8, 8),
+                        name='block5_conv1')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(8, 8),
+                        name='block5_conv2')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(8, 8),
+                        name='block5_conv3')(x)
+        # x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block5_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 6
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(16, 16),
+                        name='block6_conv1')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(16, 16),
+                        name='block6_conv2')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(16, 16),
+                        name='block6_conv3')(x)
+        # x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block6_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 7
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block7_conv1')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block7_conv2')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block7_conv3')(x)
+        # x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block6_pool')(x)
+        # x = layers.BatchNormalization()(x)
+
+        # Block 8
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block8_conv1')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block8_conv2')(x)
+        x = layers.Conv2D(512, (3, 3),
+                        activation='relu',
+                        padding='same',
+                        dilation_rate=(1, 1),
+                        name='block8_conv3')(x)
+        # x = layers.MaxPooling2D((2, 2), strides=(2, 2), name='block6_pool')(x)
+
+        x = layers.Dense(512, activation='relu')(x)
+        x= layers.Dropout(0.2)(x)
+        x = layers.BatchNormalization()(x)
+        x = layers.Dense(256, activation='relu')(x)
+        x = layers.Dropout(0.2)(x)
+        x = layers.BatchNormalization()(x)
+        # model.add(Dense(1, activation='sigmoid'))
+        x = layers.Dense(2, activation='softmax')(x)
+
+
+        if include_top:
+            # Classification block
+            x = layers.Flatten(name='flatten')(x)
+            x = layers.Dense(4096, activation='relu', name='fc1')(x)
+            x = layers.Dense(4096, activation='relu', name='fc2')(x)
+            x = layers.Dense(classes, activation='softmax', name='predictions')(x)
+        else:
+            if pooling == 'avg':
+                x = layers.GlobalAveragePooling2D()(x)
+            elif pooling == 'max':
+                x = layers.GlobalMaxPooling2D()(x)
+
+        # Ensure that the model takes into account
+        # any potential predecessors of `input_tensor`.
+        if input_tensor is not None:
+            inputs = tf.keras.utils.get_source_inputs(input_tensor)
+        else:
+            inputs = img_input
+        # Create model.
+        model = models.Model(inputs, x, name='meshnet')
+
+        WEIGHTS_PATH = '/hpcstor6/scratch01/s/sanghyuk.kim001/weights/'
+        WEIGHTS_PATH_NO_TOP = 'https://github.com/fchollet/deep-learning-models/releases/download/v0.1/'
+
+        # Load weights.
+        # if weights == 'imagenet':
+        #     if include_top:
+        #         weights_path = tf.keras.utils.get_file(
+        #             'vgg16_weights_tf_dim_ordering_tf_kernels.h5',
+        #             WEIGHTS_PATH,
+        #             cache_subdir='models',
+        #             file_hash='64373286793e3c8b2b4e3219cbf3544b')
+        #     else:
+        #         weights_path = tf.keras.utils.get_file(
+        #             'vgg16_weights_tf_dim_ordering_tf_kernels_notop.h5',
+        #             WEIGHTS_PATH_NO_TOP,
+        #             cache_subdir='models',
+        #             file_hash='6d6bbae143d832006294945121d1f1fc')
+        #     model.load_weights(weights_path)
+        #     if tf.keras.backend.backend() == 'theano':
+        #         tf.keras.utils.convert_all_kernels_in_model(model)
+        # elif weights is not None:
+        #     model.load_weights(weights)
+
+
+        # model.layers[0].trainable = False
+        model.summary()
+
+        model.compile(optimizer=self.CFG['model_optimizer'],
+                    loss=self.CFG['loss'], metrics=self.CFG['metrics'])
+
+        return model
+        def meshnet_test(self, network=None):
+            # base_model = ResNet50(include_top=False, input_shape=(
+            #     self.CFG['img_height'], self.CFG['img_width'], 3), pooling='avg', weights=self.CFG['pretrained_weights'])
+
+            # Define model with different applications
+            model = Sequential()
+            model.add(Input(shape=(150, 150, 3)))
+            # model.add(base_model)
+            
+            # image = Input(shape=(150, 150, 3))
+            # x = Conv2D(3, kernel_size=(3,3), padding='same', activation='relu')
+
+            # MeshNet-inspired layers adapted for 2D, including dilation
+            # model.add(Dense(512, activation='relu'))
+
+            # model.add(layers.Conv2D(2048,(3,3),padding='same',activation='relu'))
+            # model.add(layers.Conv2D(1024,(3,3),padding='same',activation='relu'))
+            # model.add(layers.Conv2D(512,(3,3),padding='same',activation='relu'))
+            # Layer 1: Convolutional + Relu + BatchNorm + Dropout
+            
+            model.add(layers.Conv2D(128,(3,3),padding='same',activation='relu'))
+            # model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+            model.add(Activation('relu'))
+            model.add(Dropout(0.3))
+            model.add(BatchNormalization())
+            # model.add(Dropout(0.3))
+
+            # Layers 2-6: Repetition of Convolutional + Relu + BatchNorm + Dropout with increasing dilation rates
+            """
+            Our architecture entirely consists of what [7] used as a context module but we modified it to use 3D dilated convolutions.
+            https://arxiv.org/pdf/1612.00940.pdf (MeshNet Section)
+            """
+            dilation_rates = [(1, 1), (2, 2), (4, 4), (8, 8), (16, 16)]
+            for rate in dilation_rates:
+                model.add(Conv2D(filters=64, kernel_size=(3, 3),
+                        padding='same', dilation_rate=rate))
+                model.add(Activation('relu'))
+                model.add(BatchNormalization())
+                model.add(Dropout(0.3))
+
+            # Layer 7: Convolutional + BatchNorm + Activation + Dropout
+            model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+            model.add(Activation('relu'))
+            model.add(BatchNormalization())
+            model.add(Dropout(0.3))
+
+            # Current Pipeline layers
+            # model.add(Flatten())
+            # model.add(Dense(512, activation='relu'))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Dense(256, activation='relu'))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Dense(1, activation='sigmoid'))
+
+            model.add(Conv2D(filters=64, kernel_size=(3, 3), padding='same'))
+
+            # model.add(GlobalMaxPooling2D())
+
+            # model.add(Dense(512, activation='relu'))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+            # model.add(Dense(256, activation='relu'))
+            # model.add(Dropout(0.2))
+            # model.add(BatchNormalization())
+
+            model.add(GlobalMaxPooling2D())
+            
+
+            model.add(Dense(2, activation='softmax'))
+
+            # model.layers[0].trainable = False
+
+            model.compile(optimizer=self.CFG['model_optimizer'],
+                        loss=self.CFG['loss'], metrics=self.CFG['metrics'])
+
+            model.summary()
+
+        return model
+
+    # def meshnet_dilation(input_shape, apply_softmax=True, input_tensor=None, classes):
+
+    #     if input_tensor is None:
+    #         model_in = Input(shape=input_shape)
+    #     else:
+    #         if not K.is_keras_tensor(input_tensor):
+    #             model_in = Input(tensor=input_tensor, shape=input_shape)
+    #         else:
+    #             model_in = input_tensor
+
+    #     h = Convolution2D(64, 3, 3, activation='relu', name='conv1_1')(model_in)
+    #     h = Convolution2D(64, 3, 3, activation='relu', name='conv1_2')(h)
+    #     h = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool1')(h)
+    #     h = Convolution2D(128, 3, 3, activation='relu', name='conv2_1')(h)
+    #     h = Convolution2D(128, 3, 3, activation='relu', name='conv2_2')(h)
+    #     h = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool2')(h)
+    #     h = Convolution2D(256, 3, 3, activation='relu', name='conv3_1')(h)
+    #     h = Convolution2D(256, 3, 3, activation='relu', name='conv3_2')(h)
+    #     h = Convolution2D(256, 3, 3, activation='relu', name='conv3_3')(h)
+    #     h = MaxPooling2D(pool_size=(2, 2), strides=(2, 2), name='pool3')(h)
+    #     h = Convolution2D(512, 3, 3, activation='relu', name='conv4_1')(h)
+    #     h = Convolution2D(512, 3, 3, activation='relu', name='conv4_2')(h)
+    #     h = Convolution2D(512, 3, 3, activation='relu', name='conv4_3')(h)
+    #     h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_1')(h)
+    #     h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_2')(h)
+    #     h = AtrousConvolution2D(512, 3, 3, atrous_rate=(2, 2), activation='relu', name='conv5_3')(h)
+    #     h = AtrousConvolution2D(4096, 7, 7, atrous_rate=(4, 4), activation='relu', name='fc6')(h)
+    #     h = Dropout(0.5, name='drop6')(h)
+    #     h = Convolution2D(4096, 1, 1, activation='relu', name='fc7')(h)
+    #     h = Dropout(0.5, name='drop7')(h)
+    #     h = Convolution2D(classes, 1, 1, name='final')(h)
+    #     h = ZeroPadding2D(padding=(1, 1))(h)
+    #     h = Convolution2D(classes, 3, 3, activation='relu', name='ctx_conv1_1')(h)
+    #     h = ZeroPadding2D(padding=(1, 1))(h)
+    #     h = Convolution2D(classes, 3, 3, activation='relu', name='ctx_conv1_2')(h)
+    #     h = ZeroPadding2D(padding=(2, 2))(h)
+    #     h = AtrousConvolution2D(classes, 3, 3, atrous_rate=(2, 2), activation='relu', name='ctx_conv2_1')(h)
+    #     h = ZeroPadding2D(padding=(4, 4))(h)
+    #     h = AtrousConvolution2D(classes, 3, 3, atrous_rate=(4, 4), activation='relu', name='ctx_conv3_1')(h)
+    #     h = ZeroPadding2D(padding=(8, 8))(h)
+    #     h = AtrousConvolution2D(classes, 3, 3, atrous_rate=(8, 8), activation='relu', name='ctx_conv4_1')(h)
+    #     h = ZeroPadding2D(padding=(16, 16))(h)
+    #     h = AtrousConvolution2D(classes, 3, 3, atrous_rate=(16, 16), activation='relu', name='ctx_conv5_1')(h)
+    #     h = ZeroPadding2D(padding=(1, 1))(h)
+    #     h = Convolution2D(classes, 3, 3, activation='relu', name='ctx_fc1')(h)
+    #     logits = Convolution2D(classes, 1, 1, name='ctx_final')(h)
+
+    #     if apply_softmax:
+    #         model_out = softmax(logits)
+    #     else:
+    #         model_out = logits
+
+    #     model = Model(input=model_in, output=model_out, name='dilation_camvid')
+
+    #     return model
+
+
     
     def trainData(self, train_ds, val_ds, img_height, img_width, class_names, augmentation_type, epochs=20):
         super().__init__(train_ds, val_ds, epochs)
