45fc5e4da3e7ac6971f2060a9668b47d59b1785e
SangHyuk Kim <e35889@gpu-4>
2024-09-29 11:37:34 -0700

    missing test script

--- test.py
+++ test.py
@@ -1,60 +1,50 @@
+import torch
 import melanoma as mel
 
 import itertools
 import glob
 import os
-os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
+# os.environ["HDF5_USE_FILE_LOCKING"] = "FALSE"
 import pathlib
 
+networktypes = [c.name for c in mel.NetworkType]
 
-CFG={
-    'verbose': 1,
-}
-
-img_size = (384, 384)
-
-snapshot_path = '/raid/mpsych/MELANOMA/snapshot/'
+snapshot_path = os.path.join(pathlib.Path.cwd(), 'snapshot')
+# snapshot_path = '/raid/mpsych/MELANOMA/snapshot/'
 # snapshot_path = '/hpcstor6/scratch01/s/sanghyuk.kim001/snapshot/bestperformers'
 # snapshot_path = '/hpcstor6/scratch01/s/sanghyuk.kim001/snapshot/bestperformers/ensemble'
 
-modelfiles = list(itertools.chain.from_iterable([glob.glob(f'{snapshot_path}/*_{img_size[0]}h_{img_size[1]}w_*.hdf5', recursive=True)]))
-modelnames = list(map(lambda x: pathlib.Path(os.path.basename(x)).stem, modelfiles))
+modelfiles = list(itertools.chain.from_iterable([glob.glob(f'{snapshot_path}/*/*.pt', recursive=True)]))
+modelnames = list(map(lambda x: pathlib.Path(os.path.basename(x)), modelfiles))
 # modelpath = os.path.abspath(os.path.join(modelfiles, os.pardir))
 
-networktypes = [net.name for net in mel.NetworkType]
 # modelnames.reverse()
 
+device = torch.device('cuda:6') if torch.cuda.is_available() else torch.device('cpu')
 
 
-for idx, m in enumerate(modelnames):
-    # netname = [each_m for each_m in networktypes if(each_m in m)]
-    # assert len(netname) == 1
+
+for idx, model_name in enumerate(modelnames):
+
+    db_path = f'/homes/e35889/sansa/melanoma_detection/data/melanomaDB'
+
+    # netname = [each_m for each_m in networktypes if(each_m in model)]
     # if len(netname) == 1:
-    #     mapped_netname = commondata.DBpreprocessorDict[netname[0]]
+    #     model_name = netname[0]
     # elif len(netname) == 2:
-    #     mapped_netname = commondata.DBpreprocessorDict[netname[1]]
-    # if len(netname) == 1:
-    #     netname = netname[0]
-    # elif len(netname) == 2:
-    #     netname = netname[1]
+    #     model_name = netname[1]
     # else:
     #     assert len(netname) <= 2
-    dbpath = f'/hpcstor6/scratch01/s/sanghyuk.kim001/melanomaDB/customDB/uniform01'
+    # classifier_name = pathlib.Path(modelfiles[0]).parts[-2]
+    classifier_name = pathlib.Path(modelfiles[0]).parent.name
+    
+    loaded_model = mel.CNN.load_model(model_path=modelfiles[idx], num_classes = 2, device=device)
 
-    dbpath_KaggleDB = os.path.join(dbpath, f'KaggleMB_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_HAM10000 = os.path.join(dbpath, f'HAM10000_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_ISIC2016 = os.path.join(dbpath, f'ISIC2016_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_ISIC2017 = os.path.join(dbpath, f'ISIC2017_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_ISIC2018 = os.path.join(dbpath, f'ISIC2018_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_ISIC2020 = os.path.join(dbpath, f'ISIC2020_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    dbpath_7pointcriteria = os.path.join(dbpath, f'_7_point_criteria_{img_size[0]}h_{img_size[1]}w_binary.h5')
-    # final_perf.append(base_model.evaluate_model_onAll(model_name=m, model_path=snapshot_path, network_name = netname[0], dbpath_KaggleDB=dbpath_KaggleDB, dbpath_HAM10000=dbpath_HAM10000, \
-    #     dbpath_ISIC2016=dbpath_ISIC2016, dbpath_ISIC2017=dbpath_ISIC2017, dbpath_ISIC2018=dbpath_ISIC2018, dbpath_7pointcriteria=dbpath_7pointcriteria))
+    mel.Model.evaluate_model_onAll(model=loaded_model, model_path=modelfiles[idx], db_path=db_path, device=device)
 
-    # mel.Model.evaluate_model_onAll(model_name=m, model_path=snapshot_path, dbpath_KaggleDB=dbpath_KaggleDB,\
-    #                                 dbpath_HAM10000=dbpath_HAM10000, dbpath_ISIC2016=dbpath_ISIC2016, \
-    #                                 dbpath_ISIC2017=dbpath_ISIC2017, dbpath_ISIC2018=dbpath_ISIC2018, \
-    #                                 dbpath_7pointcriteria=dbpath_7pointcriteria)
+    # Save ISIC2020 challenge submission
+    mel.Model.evaluate_leaderboard(model=loaded_model, model_path=modelfiles[idx], db_path=db_path, device=device)
+    
 
 mel.Model.extract_performances(snapshot_path)
 
